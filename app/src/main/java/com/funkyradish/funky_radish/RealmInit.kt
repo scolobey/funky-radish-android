6d26d4c6406d2b6c255e364aa4e2e542db9858f9
Ryan Goodwin <minedied@gmail.com>
2020-11-22 16:30:03 -0700

    Error getting me stuck. I'm gonna commit, jump back to the previous version and try to register with a token.

--- app/src/main/java/com/funkyradish/funky_radish/RealmInit.kt
+++ app/src/main/java/com/funkyradish/funky_radish/RealmInit.kt
@@ -1,13 +1,10 @@
 package com.funkyradish.funky_radish
 
 import android.app.Application
-import android.util.Log
-import io.realm.*
 
 class RealmInit : Application() {
 
     override fun onCreate() {
-
         super.onCreate()
         RealmService().initialize(this)
     }
